0d45d0de595e40a25f4eb000a4d03e32ec4a793b
Smart Dev <79864567+SmartDev210@users.noreply.github.com>
2021-10-28 00:33:49 +0800

    android app icon updated & authentication done

--- weavy-mobile/weavy-mobile/weavy-mobile.Android/MainActivity.cs
+++ weavy-mobile/weavy-mobile/weavy-mobile.Android/MainActivity.cs
@@ -8,7 +8,7 @@ using WeavyMobile;
 
 namespace weavy_mobile.Droid
 {
-    [Activity(Label = "weavy_mobile", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Buddy.aero", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
