84a6d45f52e63d30549b951633ecfef7ee054d3b
Smart Dev <79864567+SmartDev210@users.noreply.github.com>
2021-11-12 17:48:32 +0800

    domain updated to buddy.aero

--- weavy-mobile/weavy-mobile/weavy-mobile/Constants/Constants.cs
+++ weavy-mobile/weavy-mobile/weavy-mobile/Constants/Constants.cs
@@ -6,8 +6,8 @@ namespace WeavyMobile
 {
     public class Constants
     {
-        public const string WeavyUrl = "https://www.back-channel.com";
-        public const string ApiUrl = "https://findparts.back-channel.com";
+        public const string WeavyUrl = "https://www.buddy.aero";
+        public const string ApiUrl = "https://findparts.buddy.aero";
 
         //public const string WeavyUrl = "https://weavy-sr0.conveyor.cloud";
         //public const string ApiUrl = "https://findparts.conveyor.cloud";
